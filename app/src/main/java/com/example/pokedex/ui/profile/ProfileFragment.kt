93db03b8db40d93a4d5677df808a8d849593ccfa
“[firstname <hutapeniko@gmail.com>
2023-11-27 15:24:33 +0700

    finish logout

--- app/src/main/java/com/example/pokedex/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/pokedex/ui/profile/ProfileFragment.kt
@@ -1,11 +1,13 @@
 package com.example.pokedex.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.pokedex.LoginActivity
 import com.example.pokedex.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
@@ -35,10 +37,14 @@ class ProfileFragment : Fragment() {
         // Setup listener untuk tombol logout
         binding.btnLogout.setOnClickListener {
             profileViewModel.logout()
+            val intent = Intent(requireContext(), LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
             // Tambahkan kode untuk navigasi atau aksi setelah logout
         }
         return view
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
